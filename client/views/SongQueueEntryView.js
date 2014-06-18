a65032000af6245f239a6b6b525997af89738518
Bob Owen <bob@bobowen.org>
2014-06-18 16:43:17 -0700

    Added the views for the queue and the songs in that queue. Included them in the web page to display with the app. Allow users to click on songs in the library to add them to the queue.

--- client/views/SongQueueEntryView.js
+++ client/views/SongQueueEntryView.js
@@ -1,4 +1,15 @@
 // SongQueueEntryView.js - Defines a backbone view class for the song queue entries.
 var SongQueueEntryView = Backbone.View.extend({
-  // your code here!
+  
+  tagname: 'li',
+
+  template: _.template('(<%= artist %>) <%= title %>'),
+
+  initialize: function() {
+  },
+
+  render: function() {
+    return this.$el.html(this.template(this.model.attributes));
+  }
+
 });
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -2,9 +2,25 @@
 var SongQueueView = Backbone.View.extend({
 
   initialize: function() {
+    this.render();
+
+    this.collection.on('add remove', function(){  //TODO: Why won't this work on 'change' ?
+      this.render();
+    }, this);
   },
 
   render: function() {
+    this.$el.children().detach();
+
+    this.$el.append('<h3>Song Queue</h3>');
+    if (this.collection.length > 0) {
+      var $list = this.$el.append('<ul></ul>');
+      $list.append(
+        this.collection.map(function(song){
+          return new LibraryEntryView({model: song}).render();
+        })
+      );
+    }
     return this.$el;
   }
 
